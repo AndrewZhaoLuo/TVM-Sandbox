5cb1f634aa447aae0f0791907d6cd13739c33925
Andrew Luo <andrew.zhao.luo@gmail.com>
2021-06-18 12:31:22 -0700

    reorganize benchmark script

--- fp16_pass/benchmark_m1_mac_fp16.py
+++ fp16_pass/benchmark_m1_mac_fp16.py
@@ -22,33 +22,11 @@ import tvm
 from tvm import relay
 from tvm.driver import tvmc
 from tvm.driver.tvmc.model import TVMCModel
-from tvm.relay.transform import AMPRewrite, InferType
+from tvm.relay.transform import InferType, ToMixedPrecision, mixed_precision
 
 
 def load_model(name, **kwargs):
     return tvmc.load(path.join("./models", name), **kwargs)
-
-
-def get_one_conv_model(N=4, C_I=16, C_O=64, H=224, W=224, K=3, dtype="float32"):
-    data_shape = [N, C_I, H, W]
-    kernel_shape = [C_O, C_I, K, K]
-    data = relay.var("data", dtype=dtype, shape=data_shape)
-    weight = relay.var("weight", dtype=dtype, shape=kernel_shape)
-    conv = relay.nn.conv2d(
-        data=data,
-        weight=weight,
-        padding=(1, 1),
-        data_layout="NCHW",
-        kernel_layout="OIHW",
-        kernel_size=K,
-        channels=C_O,
-    )
-    mod = tvm.IRModule.from_expr(conv)
-    params = {
-        # "data": np.random.uniform(-1, 1, size=data_shape),
-        "weight": np.random.uniform(-1, 1, size=kernel_shape).astype(dtype),
-    }
-    return tvmc.TVMCModel(mod, params)
 
 
 def graph_optimize(tvmc_model, run_fp16_pass, run_other_opts):
@@ -72,7 +50,7 @@ def graph_optimize(tvmc_model, run_fp16_pass, run_other_opts):
 
     if run_fp16_pass:
         mod = InferType()(mod)
-        mod = AMPRewrite()(mod)
+        mod = ToMixedPrecision()(mod)
 
     if run_other_opts and run_fp16_pass:
         # run one more pass to clean up new subgraph
@@ -83,6 +61,31 @@ def graph_optimize(tvmc_model, run_fp16_pass, run_other_opts):
         mod = tvm.relay.transform.FoldConstant()(mod)
 
     return TVMCModel(mod, params)
+
+
+def get_one_conv_model(
+    N=4, C_I=16, C_O=64, H=224, W=224, K=3, run_pass=True, run_opts=True
+):
+    data_shape = [N, C_I, H, W]
+    kernel_shape = [C_O, C_I, K, K]
+    data = relay.var("data", dtype="float32", shape=data_shape)
+    weight = relay.var("weight", dtype="float32", shape=kernel_shape)
+    conv = relay.nn.conv2d(
+        data=data,
+        weight=weight,
+        padding=(1, 1),
+        data_layout="NCHW",
+        kernel_layout="OIHW",
+        kernel_size=K,
+        channels=C_O,
+    )
+    mod = tvm.IRModule.from_expr(conv)
+    params = {
+        # "data": np.random.uniform(-1, 1, size=data_shape),
+        "weight": np.random.uniform(-1, 1, size=kernel_shape).astype("float32"),
+    }
+    tvmc_model = tvmc.TVMCModel(mod, params)
+    return graph_optimize(tvmc_model, run_pass, run_opts)
 
 
 def get_distillbert(run_pass=True, run_opts=True):
@@ -105,29 +108,37 @@ def get_ssd_resnet(run_pass=True, run_opts=True):
     return graph_optimize(tvmc_model, run_pass, run_opts)
 
 
+def benchmark_model(
+    model_func,
+    run_fp16_pass=True,
+    run_other_opts=True,
+    target="llvm",
+    target_host="llvm -mcpu=apple-latest -mtriple=arm64-apple-macos",
+):
+    print("FP16 pass" if run_fp16_pass else "FP32 pass")
+    """Get Module"""
+    tvmc_model = model_func(run_pass=run_fp16_pass, run_opts=run_other_opts)
+    tuning_records = tvmc.tune(
+        tvmc_model,
+        target=target,
+        trials=100,
+        repeat=5,
+        tuner="xgb_knob",
+        target_host=target_host,
+    )
+
+    # Create package artifacts
+    package = tvmc.compile(tvmc_model, target=target, tuning_records=tuning_records)
+    result = tvmc.run(
+        package, device="cpu" if target == "llvm" else target, repeat=1000, number=1
+    )
+    print(result)
+    print()
+
+
 if __name__ == "__main__":
     # macOS has 'spawn' as default which doesn't work for tvm
     mp.set_start_method("fork")
 
-    for run_fp16_pass in [False, True]:
-        print("FP16 pass" if run_fp16_pass else "FP32 pass")
-        """Get Module"""
-        # tvmc_model = get_one_conv_model(dtype="float16")
-        # tvmc_model = get_one_conv_model(dtype="float32")
-        # tvmc_model = get_distillbert()
-        # tvmc_model = get_bert(run_pass=True)
-        tvmc_model = get_yolo2(run_pass=run_fp16_pass)
-        # tvmc_model = get_ssd_resnet(run_pass=run_fp16_pass)
-        # tvmc_model.summary()
-
-        # Create tuning artifacts
-        target = "llvm -mcpu=apple-latest -mtriple=arm64-apple-macos"
-        tuning_records = tvmc.tune(
-            tvmc_model, target=target, trials=10000, repeat=5, tuner="xgb_knob"
-        )
-
-        # Create package artifacts
-        package = tvmc.compile(tvmc_model, target=target, tuning_records=tuning_records)
-        result = tvmc.run(package, device="cpu", repeat=1000, number=1)
-        print(result)
-        print()
+    benchmark_model(get_bert, run_fp16_pass=True, run_other_opts=True, target="metal")
+    benchmark_model(get_bert, run_fp16_pass=False, run_other_opts=True, target="metal")
