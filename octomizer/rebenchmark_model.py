6055ee98cccb6ec5bd61b78b0eead0bfdc564ec7
Andrew Zhao Luo <andrewzhaoluo@system76-pc.localdomain>
2021-10-01 13:53:25 -0700

    rebenchmark models

---
+++ octomizer/rebenchmark_model.py
@@ -0,0 +1,23 @@
+import octomizer
+import octomizer.client
+
+client = octomizer.client.OctomizerClient()
+
+models = client.list_models()
+
+# T4 AWS + GCP targets
+platforms = ["aws_g4dn.xlarge", "nvidia_tesla_t4"]
+
+for model in models:
+    model_variants = list(model.list_model_variants())
+    print(f"Model: {model.uuid}")
+    for variant in model_variants:
+        print(f"\tVariant: {variant.uuid}")
+        for platform in platforms:
+            print(f"\t\tBenchmarking on platform {platform}")
+            workflow = variant.benchmark(platform=platform, num_benchmark_trials=10, num_runs_per_trial=100)
+            workflow.wait()
+            if not workflow.completed():
+                print(f"\t\t\tWorkflow failed with status {workflow.status()}")
+            metrics = benchmark_workflow.metrics()
+            print(f"\t\t\tMetrics:\n{metrics}")
