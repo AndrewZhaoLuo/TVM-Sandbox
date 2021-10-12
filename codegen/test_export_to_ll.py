4f16f6736b949469835dc3a1c43a111d00165858
Andrew Zhao Luo <andrewzhaoluo@system76-pc.localdomain>
2021-10-12 15:34:26 -0700

    update tutorial

--- codegen/test_export_to_ll.py
+++ codegen/test_export_to_ll.py
@@ -6,14 +6,13 @@ import tvm
 from tvm import te
 
 if __name__ == "__main__":
-    # For testing branch aluo/codegen/llvm-fastmath
-    os.environ["TVM_FAST_MATH_FLAG"] = "0"
+    TARGET = "llvm -O=3 --fast-math"
 
-    n = 2
+    n = 128
     A = te.placeholder((n,), name="A", dtype="float32")
     B = te.placeholder((n,), name="B", dtype="float32")
     C = te.compute(A.shape, lambda *i: A(*i) + B(*i), name="C")
     s = tvm.te.create_schedule(C.op)
     m = tvm.lower(s, [A, B, C], name="test_add")
-    rt_mod = tvm.build(m, target="llvm")
+    rt_mod = tvm.build(m, target=TARGET)
     rt_mod.save("test.ll")
