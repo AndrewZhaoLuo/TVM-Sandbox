2540ce997936fed22c06548c716a4eb31e217b50
Andrew Luo <andrew.zhao.luo@gmail.com>
2022-01-24 11:25:30 -0800

    debug script

--- onnx/debug_caffenet_int8_import.py
+++ onnx/debug_caffenet_int8_import.py
@@ -10,4 +10,15 @@ model_path = "models/caffenet-12-int8.onnx"
 onnx_model = onnx.load(model_path)
 shape_dict = {'data_0': (1, 3, 224, 224)}
 
+# should be successful but fails with:
+"""
+The Relay type checker is unable to show the following types match:
+  Tensor[(1), float32]
+  Tensor[(96), float32]
+In particular:
+  dimension 0 conflicts: 1 does not match 96.
+The Relay type checker is unable to show the following types match.
+In particular `Tensor[(96), float32]` does not match `Tensor[(1), float32]`
+note: run with `TVM_BACKTRACE=1` environment variable to display a backtrace.
+"""
 mod, params = relay.frontend.from_onnx(onnx_model, shape_dict)
