7d7d0e2580a181155f41b00703d6ce8139f3703f
Andrew Luo <andrew.zhao.luo@gmail.com>
2022-01-25 22:54:53 -0800

    examine list all ops

--- relay/example_list_all_ops.py
+++ relay/example_list_all_ops.py
@@ -1,10 +1,18 @@
 """
-How to list all ops registered so far.
+How to list all ops registered so far for quantization and in a generic registry.
 """
 
 import tvm
 
 op_names = tvm.ir.Op.list_op_names()
 
-for name in op_names:
+print("Ops:")
+for name in sorted(op_names):
     print(name)
+print()
+
+print("Those with registered FTVMFakeQuantizationToInteger")
+for name in sorted(op_names):
+    op = tvm.ir.Op.get(name)
+    if op.get_attr("FTVMFakeQuantizationToInteger"):
+        print(name)
