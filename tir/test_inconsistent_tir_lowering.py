19284ddbd6bb28af61c0c2aa8bb334c5c53731a7
Andrew Luo <andrew.zhao.luo@gmail.com>
2022-03-17 11:31:03 -0700

    inconstitent tir lowering

--- tir/test_inconsistent_tir_lowering.py
+++ tir/test_inconsistent_tir_lowering.py
@@ -5,7 +5,7 @@ import tvm
 from tvm import auto_scheduler, te, topi
 from tvm.ir.base import save_json
 
-LOG_LINE = '{"i": [["[\\"conv2d_layer\\", 1, 7, 7, 512, 512, 3, 3, [1, 1], [1, 1]]", "llvm -keys=cpu -link-params=0 -mcpu=broadwell -num-cores=2", [8, 64, 64, 0, 0, 0, 0, 0], "", 1, []], [[], [["CI", 5], ["SP", 3, 0, 1, [1, 1, 1], 1], ["SP", 3, 4, 512, [2, 1, 1], 1], ["SP", 3, 8, 7, [1, 1, 1], 1], ["SP", 3, 12, 7, [1, 7, 1], 1], ["SP", 3, 16, 512, [32], 1], ["SP", 3, 18, 3, [1], 1], ["SP", 3, 20, 3, [3], 1], ["RE", 3, [0, 4, 8, 12, 1, 5, 9, 13, 16, 18, 20, 2, 6, 10, 14, 17, 19, 21, 3, 7, 11, 15]], ["FSP", 6, 0, 1, 1], ["FSP", 6, 2, 2, 1], ["FSP", 6, 4, 3, 1], ["FSP", 6, 6, 4, 1], ["RE", 6, [0, 2, 4, 6, 1, 3, 5, 7]], ["CA", 3, 6, 3], ["CR", 1], ["FU", 1, [0, 1]], ["AN", 1, 0, 3], ["FU", 6, [0, 1]], ["AN", 6, 0, 3], ["PR", 3, 0, "auto_unroll_max_step$16"], ["AN", 1, 2, 2], ["AN", 3, 21, 2], ["AN", 6, 6, 2]]]], "r": [[0.0279606], 0, 0.848875, 1647464340], "v": "v0.6"}\n'
+LOG_LINE = '{"i": [["[\\"conv2d_layer\\", 1, 7, 7, 512, 512, 3, 3, [1, 1], [1, 1]]", "llvm -keys=cpu -link-params=0 -mcpu=broadwell -num-cores=2", [8, 64, 64, 0, 0, 0, 0, 0], "", 1, []], [[], [["CI", 5], ["SP", 3, 0, 1, [1, 1, 1], 1], ["SP", 3, 4, 512, [1, 32, 16], 1], ["SP", 3, 8, 7, [7, 1, 1], 1], ["SP", 3, 12, 7, [1, 1, 1], 1], ["SP", 3, 16, 512, [1], 1], ["SP", 3, 18, 3, [1], 1], ["SP", 3, 20, 3, [3], 1], ["RE", 3, [0, 4, 8, 12, 1, 5, 9, 13, 16, 18, 20, 2, 6, 10, 14, 17, 19, 21, 3, 7, 11, 15]], ["FSP", 6, 0, 1, 2], ["FSP", 6, 3, 2, 2], ["FSP", 6, 6, 3, 2], ["FSP", 6, 9, 4, 2], ["RE", 6, [0, 3, 6, 9, 1, 4, 7, 10, 2, 5, 8, 11]], ["CA", 3, 6, 7], ["CA", 1, 6, 5], ["FU", 6, [0, 1, 2, 3, 4, 5]], ["AN", 6, 0, 3], ["PR", 3, 0, "auto_unroll_max_step$512"], ["AN", 1, 3, 2], ["AN", 3, 21, 2], ["AN", 6, 6, 2]]]], "r": [[0.0331129], 0, 0.900362, 1647464342], "v": "v0.6"}\n'
 
 # The workload associated with the log
 @auto_scheduler.register_workload
@@ -23,9 +23,9 @@ def conv2d_layer(N, H, W, CO, CI, KH, KW, stride, padding):
 if __name__ == "__main__":
     inp, inr = auto_scheduler.measure_record.load_record_from_string(LOG_LINE)
     inp = auto_scheduler.measure.recover_measure_input(inp, rebuild_state=True)
-    sch, args = inp.task.compute_dag.apply_steps_from_state(inp.state)
 
     for _ in range(10):
+        sch, args = inp.task.compute_dag.apply_steps_from_state(inp.state)
         ir_module = tvm.lower(sch, args)
         primfunc = ir_module["main"]
         json_str = save_json(primfunc)
