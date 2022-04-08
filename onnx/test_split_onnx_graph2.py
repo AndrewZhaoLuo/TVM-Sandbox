086d4135978acca622da599363f7584cf971073c
Andrew Luo <andrew.zhao.luo@gmail.com>
2022-04-08 01:11:06 -0700

    fixins

--- onnx/test_split_onnx_graph2.py
+++ onnx/test_split_onnx_graph2.py
@@ -20,99 +20,6 @@ OPERATORS = {"Conv", "MatMul"}
 DYNAMIC_OPS = {"Reshape": (1,)}
 
 
-def update_inputs_outputs_dims(
-    model, input_dims, output_dims
-):  # type: (ModelProto, Dict[Text, List[Any]], Dict[Text, List[Any]]) -> ModelProto
-    """
-    Copy from from onnx.tools import update_model_dims with one diff
-
-    This function updates the dimension sizes of the model's inputs and outputs to the values
-    provided in input_dims and output_dims. if the dim value provided is negative, a unique dim_param
-    will be set for that dimension.
-
-    Example. if we have the following shape for inputs and outputs:
-            shape(input_1) = ('b', 3, 'w', 'h')
-            shape(input_2) = ('b', 4)
-            and shape(output)  = ('b', 'd', 5)
-
-        The parameters can be provided as:
-            input_dims = {
-                "input_1": ['b', 3, 'w', 'h'],
-                "input_2": ['b', 4],
-            }
-            output_dims = {
-                "output": ['b', -1, 5]
-            }
-
-        Putting it together:
-            model = onnx.load('model.onnx')
-            updated_model = update_inputs_outputs_dims(model, input_dims, output_dims)
-            onnx.save(updated_model, 'model.onnx')
-    """
-    dim_param_set = set()  # type: Set[Text]
-
-    def init_dim_param_set(
-        dim_param_set, value_infos
-    ):  # type: (Set[Text], List[ValueInfoProto]) -> None
-        for info in value_infos:
-            shape = info.type.tensor_type.shape
-            for dim in shape.dim:
-                if dim.HasField("dim_param"):
-                    dim_param_set.add(dim.dim_param)  # type: ignore
-
-    init_dim_param_set(dim_param_set, model.graph.input)  # type: ignore
-    init_dim_param_set(dim_param_set, model.graph.output)  # type: ignore
-    init_dim_param_set(dim_param_set, model.graph.value_info)  # type: ignore
-
-    def update_dim(
-        tensor, dim, j, name
-    ):  # type: (ValueInfoProto, Any, int, Text) -> None
-        dim_proto = tensor.type.tensor_type.shape.dim[j]
-        if isinstance(dim, int):
-            if dim >= 0:
-                if dim_proto.HasField("dim_value") and dim_proto.dim_value != dim:
-                    raise ValueError(
-                        "Unable to set dimension value to {} for axis {} of {}. Contradicts existing dimension value {}.".format(
-                            dim, j, name, dim_proto.dim_value
-                        )
-                    )
-                dim_proto.dim_value = dim
-            else:
-                generated_dim_param = name + "_" + str(j)
-                if generated_dim_param in dim_param_set:
-                    raise ValueError(
-                        "Unable to generate unique dim_param for axis {} of {}. Please manually provide a dim_param value.".format(
-                            j, name
-                        )
-                    )
-                dim_proto.dim_param = generated_dim_param
-        elif isinstance(dim, string_types):
-            dim_proto.dim_param = dim
-        else:
-            raise ValueError(
-                "Only int or str is accepted as dimension value, incorrect type: {}".format(
-                    type(dim)
-                )
-            )
-
-    for input in model.graph.input:
-        input_name = input.name
-        if input_name in input_dims:
-            input_dim_arr = input_dims[input_name]
-            for j, dim in enumerate(input_dim_arr):
-                update_dim(input, dim, j, input_name)
-
-    for output in model.graph.output:
-        output_name = output.name
-        if output_name in output_dims:
-            output_dim_arr = output_dims[output_name]
-            for j, dim in enumerate(output_dim_arr):
-                update_dim(output, dim, j, output_name)
-
-    onnx.checker.check_model(model)
-    return model
-
-
 class SubgraphExtractor:
     def __init__(
         self,
@@ -120,13 +27,6 @@ class SubgraphExtractor:
         input_values: Dict[str, np.ndarray],
         operators_of_interest: Set[str] = OPERATORS,
     ) -> None:
-        # This doesn't work too well with some models. Future Idea: run model to get shapes
-        # Set shapes so each component is able to be calculated with real shapes
-        onnx_model = update_inputs_outputs_dims(
-            onnx_model,
-            {k: v.shape for k, v in input_values.items()},
-            {},
-        )
         self.onnx_model = onnx.shape_inference.infer_shapes(onnx_model)
         self.input_values = input_values
         self.operators_of_interest = set(operators_of_interest)
