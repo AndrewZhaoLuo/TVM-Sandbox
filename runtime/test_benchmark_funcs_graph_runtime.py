6e8a2465adf8e8fbba7e918e5e65369f404c1216
Andrew Zhao Luo <andrew.zhao.luo@gmail.com>
2022-04-18 16:15:47 -0700

    example of using graph debugger

---
+++ runtime/test_benchmark_funcs_graph_runtime.py
@@ -0,0 +1,60 @@
+"""Should work after https://github.com/apache/tvm/pull/11000 merges. 
+
+Benchmarks individual nodes in the graph runtime.
+"""
+
+from tvm import relay
+import onnx
+
+import numpy as np
+
+import tvm
+from tvm.contrib.debugger import debug_executor
+import numpy as np
+
+onnx_model = onnx.load("models/shufflenet-9.onnx")
+
+# Build the model, get a VMExecutor.
+mod, params = relay.frontend.from_onnx(
+    onnx_model,
+    freeze_params=True,
+)
+target = "llvm"
+
+with tvm.transform.PassContext(opt_level=3, config={"relay.FuseOps.max_depth": 1}):
+    lib = relay.build(mod, target=target, params=params)
+
+dev = tvm.device(target, 0)
+rt_mod = debug_executor.GraphModuleDebug(
+    lib["debug_create"]("default", dev),
+    [dev],
+    lib.get_graph_json(),
+    dump_root="profiling_data/",
+)
+
+# Set inputs here, examine model/look at info in config
+input_dict = {"gpu_0/data_0": np.zeros((1, 3, 224, 224), dtype="float32")}
+rt_mod.set_input(**input_dict)
+
+node_name_to_out_shapes = {}
+for node in rt_mod.debug_datum.get_graph_nodes():
+    node_name_to_out_shapes[node["name"]] = node["shape"]
+
+for i, node in enumerate(rt_mod.debug_datum.get_graph_nodes()):
+    result = rt_mod.run_individual_node(i, 10, 10, min_repeat_ms=100)
+
+    node_name = node["name"]
+    output_shape = str(node["shape"])
+
+    input_shapes = []
+    for input_node_name in node.get("inputs", []):
+        input_shapes.append(node_name_to_out_shapes[input_node_name])
+    input_shapes = str(input_shapes)
+
+    result_arr = np.array(result).flatten()
+    result_avg = np.average(result_arr).item()
+    result_std = np.std(result_arr).item()
+    print(
+        f"{i: <4} {node_name: <60} out: {output_shape: <20} in: {input_shapes: <60}"
+        f"-- {result_avg} ({result_std})"
+    )
