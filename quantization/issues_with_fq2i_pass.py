b90010bb4d860426fa469241d34fe69132c4b160
Andrew Zhao Luo <andrew.zhao.luo@gmail.com>
2022-09-21 13:01:36 -0700

    add misc quantization

--- quantization/issues_with_fq2i_pass.py
+++ quantization/issues_with_fq2i_pass.py
@@ -4,31 +4,44 @@ from tvm import relay
 from tvm import IRModule
 import tvm
 
+# scale and zero point for hte layer
+SF = relay.const(0.025, dtype="float32")
+ZP = relay.const(1, dtype="int32")
+
 
 def relay_special_residual_layer_fake_quantized(
     input_var: relay.Var,
 ):
-    # scale and zero point for hte layer
-    sf = relay.const(0.025, dtype="float32")
-    zp = relay.const(1, dtype="int32")
-
-    q_input = relay.qnn.op.quantize(input_var, sf, zp)
-    qdq_input = relay.qnn.op.dequantize(q_input, sf, zp)
+    q_input = relay.qnn.op.quantize(input_var, SF, ZP)
+    qdq_input = relay.qnn.op.dequantize(q_input, SF, ZP)
 
     # Potentially expensive op!
     expensive_mean = relay.mean(qdq_input, axis=-1, keepdims=True)
 
     # To preserve shape
     diamond_mul = relay.multiply(expensive_mean, qdq_input)
+    sqrt = relay.sqrt(diamond_mul)
 
-    q_diamond = relay.qnn.op.quantize(diamond_mul, sf, zp)
-    qdq_diamond = relay.qnn.op.dequantize(q_diamond, sf, zp)
+    q_diamond = relay.qnn.op.quantize(sqrt, SF, ZP)
+    qdq_diamond = relay.qnn.op.dequantize(q_diamond, SF, ZP)
 
     # This is the problematic operation, in subgraph extraction, we don't have
     # a handle into the quantized version of the previous nodes...
     # This eventually duplicates the expensive operations in the diamond
-    evil_add = relay.add(qdq_diamond, diamond_mul)
+    evil_add = relay.add(qdq_diamond, sqrt)
     return evil_add
+
+
+def expected_result_quantized(input_var: relay.Var):
+    # input_var shoudl already be quantized
+    cast_int32 = relay.cast(input_var, "int32")
+    mean_int32 = relay.mean(cast_int32, axis=-1, keepdims=True)
+    mean_result = relay.cast(mean_int32, "int8")
+
+    mul_result = relay.qnn.op.mul(mean_result, input_var, SF, ZP, SF, ZP, SF, ZP)
+    sqrt_result = relay.qnn.op.sqrt(mul_result, SF, ZP, SF, ZP)
+    add = relay.qnn.op.add(mul_result, sqrt_result, SF, ZP, SF, ZP, SF, ZP)
+    return add
 
 
 def generate_evil_model(shape=[1, 512, 512], layers=10) -> IRModule:
@@ -37,6 +50,19 @@ def generate_evil_model(shape=[1, 512, 512], layers=10) -> IRModule:
     cur_last_expr = input_var
     for _ in range(layers):
         cur_last_expr = relay_special_residual_layer_fake_quantized(cur_last_expr)
+
+    mod = IRModule.from_expr(cur_last_expr)
+    mod = relay.transform.InferType()(mod)
+    return mod
+
+
+def generated_expected_model(shape=[1, 512, 512], layers=10) -> IRModule:
+    input_var = relay.var("input_var", shape=shape)
+    input_var_quantized = relay.qnn.op.quantize(input_var, SF, ZP)
+
+    cur_last_expr = input_var_quantized
+    for _ in range(layers):
+        cur_last_expr = expected_result_quantized(cur_last_expr)
 
     mod = IRModule.from_expr(cur_last_expr)
     mod = relay.transform.InferType()(mod)
@@ -63,7 +89,8 @@ def run_fq2i(mod: IRModule) -> IRModule:
 
 
 if __name__ == "__main__":
-    mod = generate_evil_model(layers=1)
+    mod = generate_evil_model(layers=3)
+    expected_mod = generated_expected_model(layers=3)
 
     # This should duplicate the diamond branch :'(
     print("BEFORE")
@@ -71,3 +98,5 @@ if __name__ == "__main__":
     print()
     print("AFTER")
     print(run_fq2i(mod))
+    print("Expected")
+    print(expected_mod)
